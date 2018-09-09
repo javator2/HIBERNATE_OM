a313541cf0775c415bc2e1a4bd130f610f781636
Olek <oleksiy.mysko@gmail.com>
2018-09-09 12:17:18 +0200

    add first book

--- src/main/java/com/sda/hibernate/Main.java
+++ src/main/java/com/sda/hibernate/Main.java
@@ -1,5 +1,7 @@
 package com.sda.hibernate;
 
+import com.sda.hibernate.entity.Book;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -15,8 +17,21 @@ public class Main {
             throw new ExceptionInInitializerError(ex);
         }
     }
+        public static Session getSession(){
+            return sessionFactory.openSession();
+        }
+
+
 
     public static void main(String[] args) {
+
+        Book book = new Book();
+        book.setAuthor("Jan");
+        book.setIsbn("324-234");
+        book.setTytle("Nowe");
+        Session session = getSession();
+        session.save(book);
+        session.close();
 
     }
 }
---
+++ src/main/java/com/sda/hibernate/entity/Category.java
@@ -0,0 +1,34 @@
+package com.sda.hibernate.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column (length = 50, nullable = false, unique = true)
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
